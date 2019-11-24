a2456c72a89cb69f7756a25236651a4e2e124cef
Noh Hyeon Jin <hjforj@naver.com>
2019-11-24 16:25:53 +0900

    Updates

---
+++ precache-manifest.73aeea28759373a0bca3c8306d8c4235.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29802701c4c0266988fc1c15f95af2ca",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "888b7130ca9ac80e338b",
+    "url": "/movie_app_2019/static/js/2.85b7fbe9.chunk.js"
+  },
+  {
+    "revision": "2f4b2211a7b1301047cb",
+    "url": "/movie_app_2019/static/js/main.2609446d.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
