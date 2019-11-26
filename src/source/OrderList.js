6691b76915209b3bc268ba911aaf6b9b98e7c640
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-11-26 15:17:41 +0900

    Update OrderList.js

--- src/source/OrderList.js
+++ src/source/OrderList.js
@@ -1,4 +1,4 @@
-import React, {useState, Component} from "react";
+import React, {useState, useEffect} from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Drawer from "@material-ui/core/Drawer";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -33,21 +33,52 @@ const GET_STORE_ORDER_LIST = gql`
   }
 `;
 
-const handleNotifyDelivery = async () => {
-
-}
-
+const SUBSCRIBE_STORE_ORDER = gql`
+  supscription subscriptStoreOrder($storeId: String!) {
+    subscriptStoreOrder(storeId: $storeId) {
+      id
+      address
+      menuList{
+        menu{
+          name
+        }
+        quantity
+      }
+      totalPrice
+      chatRoom{
+        state
+      }
+    }
+  }
+`;
 
 const OrderList = () => {
  
   const [toggle, setToggle ] = useState(1);
 
   const StoreID = window.localStorage.getItem('id');
-  const { data:  storeOrderList , loading } = useQuery(GET_STORE_ORDER_LIST, {
+  const { data: { getStoreOrderList }, loading } = useQuery(GET_STORE_ORDER_LIST, {
     variables: {
       storeId : StoreID
     }
   });
+  const [ storeOrderList, setStoreOrderList ] = useState(getStoreOrderList || []);
+  const { data } = useSubscription(SUBSCRIBE_STORE_ORDER, {
+    variables: {
+      storeId: StoreID
+    }
+  });
+
+  const handleNewStoreOrder = () => {
+    if(data !== undefined) {
+      const { subscriptStoreOrder } = data;
+      setStoreOrderList(previous => [...previous, subscriptStoreOrder]);
+    }
+  }
+
+  useEffect(() => {
+    handleNewStoreOrder();
+  }, [data]);
 
   const handleDrawerToggle = async () => {
     if(toggle === 0)
@@ -63,45 +94,44 @@ const OrderList = () => {
   const handlePressStoreScreen = () => {
     window.location.assign('/StoreManagement'); //페이지 넘기기
   }
-    return (
-      loading?
-      <div>
-         <img src={logo} alt="Logo" width="100px" height="auto" />
-      </div>:
-      <div className="root">
-        <AppBar position="static">
-          <IconButton
-            className="menuButton"
-            color="inherit"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon/>
-          </IconButton>
-        </AppBar>
-        <Drawer open={toggle === 0} >
-          <MenuItem onClick={handlePressOrderScreen}>order management</MenuItem>
-          <MenuItem onClick={handlePressStoreScreen}>store management</MenuItem>
-        </Drawer>
-        <div className="menu_list">
-        { storeOrderList.getStoreOrderList.length === 0?
-          <p>주문내역이 없습니다!</p>
-          :
-        storeOrderList.getStoreOrderList.map(item =>{
+  return (
+    loading?
+    <div>
+        <img src={logo} alt="Logo" width="100px" height="auto" />
+    </div>:
+    <div className="root">
+      <AppBar position="static">
+        <IconButton
+          className="menuButton"
+          color="inherit"
+          onClick={handleDrawerToggle}
+        >
+          <MenuIcon/>
+        </IconButton>
+      </AppBar>
+      <Drawer open={toggle === 0} >
+        <MenuItem onClick={handlePressOrderScreen}>order management</MenuItem>
+        <MenuItem onClick={handlePressStoreScreen}>store management</MenuItem>
+      </Drawer>
+      <div className="menu_list">
+      {
+        storeOrderList.length === 0?
+        <p>주문내역이 없습니다!</p>
+        :
+        storeOrderList.map(item =>{
           return <Order
-              key={item.id}
-              roomId={item.id}
-              location={item.address}
-              menuList={item.menuList}
-              price={item.totalPrice}
-              chatRoom={item.chatRoom.state}
-            />;
-        })};
-         
-        </div>
+            key={item.id}
+            roomId={item.id}
+            location={item.address}
+            menuList={item.menuList}
+            price={item.totalPrice}
+            chatRoom={item.chatRoom.state}
+          />;
+      })};
+        
       </div>
-    );
-  }
-
-
+    </div>
+  );
+}
 
 export default OrderList;
