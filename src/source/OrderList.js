950d25883850cbc3c0ed760372631ade18ce192d
Noh Hyeon Jin <hjforj@naver.com>
2019-11-26 15:29:16 +0900

    Revert "OrderList 화면 전환 추가"
    This reverts commit 599dece48387df52eb213451217790dd203493fa.

--- src/source/OrderList.js
+++ src/source/OrderList.js
@@ -1,16 +1,15 @@
-import React, {useState, Component} from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Drawer from "@material-ui/core/Drawer";
 import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import gql from "graphql-tag";
+import { useQuery, useSubscription } from "react-apollo-hooks";
+
 import '../css/OrderList.css'
 import logo from "../image/logo.png";
-
-import gql from "graphql-tag";
-import { useQuery } from "react-apollo-hooks";
 import Order from './Order';
-
 
 //graphQL queries
 
@@ -33,75 +32,98 @@ const GET_STORE_ORDER_LIST = gql`
   }
 `;
 
-const handleNotifyDelivery = async () => {
-
-}
-
+const SUBSCRIBE_STORE_ORDER = gql`
+  supscription subscriptStoreOrder($storeId: String!) {
+    subscriptStoreOrder(storeId: $storeId) {
+      id
+      address
+      menuList{
+        menu{
+          name
+        }
+        quantity
+      }
+      totalPrice
+      chatRoom{
+        state
+      }
+    }
+  }
+`;
 
 const OrderList = () => {
- 
-  const [toggle, setToggle ] = useState(1);
+  const [ toggle, setToggle ] = useState(1);
 
   const StoreID = window.localStorage.getItem('id');
-  const { data:  storeOrderList , loading } = useQuery(GET_STORE_ORDER_LIST, {
+  const { data: { getStoreOrderList }, loading } = useQuery(GET_STORE_ORDER_LIST, {
     variables: {
       storeId : StoreID
     }
   });
+  const [ storeOrderList, setStoreOrderList ] = useState(getStoreOrderList || []);
+  const { data } = useSubscription(SUBSCRIBE_STORE_ORDER, {
+    variables: {
+      storeId: StoreID
+    }
+  });
 
-  const handleDrawerToggle = async () => {
-    if(toggle === 0)
-    {
-      setToggle(1)
-    }else{
-      setToggle(0)
+  const handleNewStoreOrder = () => {
+    if(data !== undefined) {
+      const { subscriptStoreOrder } = data;
+      setStoreOrderList(previous => [...previous, subscriptStoreOrder]);
     }
   }
-  const handlePressOrderScreen = () => {
-    window.location.assign('/OrderList'); //페이지 넘기기
-  }
-  const handlePressStoreScreen = () => {
-    window.location.assign('/StoreManagement'); //페이지 넘기기
-  }
-    return (
-      loading?
-      <div>
-         <img src={logo} alt="Logo" width="100px" height="auto" />
-      </div>:
-      <div className="root">
-        <AppBar position="static">
-          <IconButton
-            className="menuButton"
-            color="inherit"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon/>
-          </IconButton>
-        </AppBar>
-        <Drawer open={toggle === 0} >
-          <MenuItem onClick={handlePressOrderScreen}>order management</MenuItem>
-          <MenuItem onClick={handlePressStoreScreen}>store management</MenuItem>
-        </Drawer>
-        <div className="menu_list">
-        { storeOrderList.getStoreOrderList.length === 0?
-          <p>주문내역이 없습니다!</p>
-          :
-        storeOrderList.getStoreOrderList.map(item =>{
+
+  useEffect(() => {
+    handleNewStoreOrder();
+  }, [data]);
+
+  const handleDrawerToggle = async () => {
+    if(toggle === 0) {
+      setToggle(1)
+    } else {
+      setToggle(0)
+    }
+  };
+
+  return (
+    loading ?
+    <div>
+        <img src={logo} alt="Logo" width="100px" height="auto" />
+    </div>:
+    <div className="root">
+      <AppBar position="static">
+        <IconButton
+          className="menuButton"
+          color="inherit"
+          onClick={handleDrawerToggle}
+        >
+          <MenuIcon/>
+        </IconButton>
+      </AppBar>
+      <Drawer open={toggle === 0} >
+        <MenuItem onClick={handleDrawerToggle}>order management</MenuItem>
+        <MenuItem onClick={handleDrawerToggle}>store management</MenuItem>
+      </Drawer>
+      <div className="menu_list">
+      {
+        storeOrderList.length === 0 ?
+        <p>주문내역이 없습니다!</p>
+        :
+        storeOrderList.map(item =>{
           return <Order
-              key={item.id}
-              roomId={item.id}
-              location={item.address}
-              menuList={item.menuList}
-              price={item.totalPrice}
-              chatRoom={item.chatRoom.state}
-            />;
-        })};
-         
-        </div>
+            key={item.id}
+            roomId={item.id}
+            location={item.address}
+            menuList={item.menuList}
+            price={item.totalPrice}
+            chatRoom={item.chatRoom.state}
+          />;
+      })};
+        
       </div>
-    );
-  }
-
-
+    </div>
+  );
+}
 
 export default OrderList;
